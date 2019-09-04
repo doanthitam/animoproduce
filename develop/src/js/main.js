f4f4e311615aa7ba87ae1c546bcb500ec9d4041e
user1 <doantam8924@gmail.com>
2019-09-04 17:58:31 +0900

    Load function

--- develop/src/js/main.js
+++ develop/src/js/main.js
@@ -61,29 +61,43 @@ $(function(){
   });
 });
 
+
+var $delayTime = 1000;
+ $(window).on('load', function() {
+   var $loadingAnim = $('#loadingAnim'),
+       $body = $('body');
+
+     setTimeout( function(){
+
+       $body.addClass('loaded');
+
+       $loadingAnim.find('.loadingAnim_line').on('transitionend', function( e ){
+         $(this).parent().remove(); }); }, $delayTime );
+ });
+
 //anime
-(function($){
-	$.fn.scrollClass = function(config){
-		var defaults = {};
-		var config = $.extend(defaults, config);
-		var target = this;
-		function addAction(){
-			var length = target.length;
-			for(var i=0; i<length; i++){
-				if(target.eq(i).hasClass('action')) continue;
-
-				var in_position = target.eq(i).offset().top;
-				var window_bottom_position = $(window).scrollTop() + $(window).height();
-				if(in_position < window_bottom_position){
-					target.eq(i).addClass('action');
-				}
-			}
-		}
-		addAction();
-
-		$(window).on('scroll', $.throttle(250, function(){
-			addAction();
-		}));
-		return target;
-	};
-} )(jQuery);
+// (function($){
+// 	$.fn.scrollClass = function(config){
+// 		var defaults = {};
+// 		var config = $.extend(defaults, config);
+// 		var target = this;
+// 		function addAction(){
+// 			var length = target.length;
+// 			for(var i=0; i<length; i++){
+// 				if(target.eq(i).hasClass('action')) continue;
+//
+// 				var in_position = target.eq(i).offset().top;
+// 				var window_bottom_position = $(window).scrollTop() + $(window).height();
+// 				if(in_position < window_bottom_position){
+// 					target.eq(i).addClass('action');
+// 				}
+// 			}
+// 		}
+// 		addAction();
+//
+// 		$(window).on('scroll', $.throttle(250, function(){
+// 			addAction();
+// 		}));
+// 		return target;
+// 	};
+// } )(jQuery);
